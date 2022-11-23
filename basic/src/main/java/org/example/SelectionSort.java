c185b1bf312f0ecc7896bbcd6bebdd8c4910b589
yanggeng <yanggeng@mfexcel.com>
2022-11-23 13:26:15 +0800

    选择排序-完整算法

--- basic/src/main/java/org/example/SelectionSort.java
+++ basic/src/main/java/org/example/SelectionSort.java
@@ -5,27 +5,27 @@ public class SelectionSort {
     public static void main(String[] args){
         int[] arr = {5,3,6,8,1,7,9,4,2};
 
-        int minPos = 0;
-        //找到最小值的下标位置
-        for (int j = 1; j < arr.length; j++) {
-            if(arr[j] < arr[minPos]){
-                minPos = j;
+        for (int i = 0; i < arr.length; i++) {
+            int minPos = i;
+            //找到最小值的下标位置
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[j] < arr[minPos]){
+                    minPos = j;
+                }
             }
+            System.out.println("minPos 的下标：" + minPos);
+
+            int tmp = arr[i];
+            arr[i] =  arr[minPos];
+            arr[minPos] = tmp;
         }
 
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("minPos 的下标：" + minPos);
+        print(arr);
+    }
 
-        int tmp = arr[0];
-        arr[0] =  arr[minPos];
-        arr[minPos] = tmp;
-
+    static void print(int[] arr){
         for (int i : arr) {
             System.out.print(i + " ");
         }
     }
 }
-
